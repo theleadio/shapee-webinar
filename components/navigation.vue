90efc41833d670bf40dca7c4c1d5360d3eb058f5
Edmund Hee <edmund.hee05@gmail.com>
2020-12-02 16:50:43 +0800

    Updated with no code

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="w-screen py-4 px-4 bg-orange-600">
-      <div class="text-white font-light text-3xl tracking-wide font-poppins">
-          Shapee
-      </div>
-  </div>
+    <div></div>
 </template>
 
 <script>
